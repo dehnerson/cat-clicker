bea0ce475b2a2d662df689d56341aac6e3b728dc
Sascha Dehler <saschadehler@gmail.com>
2018-05-08 14:47:49 +0200

    Added second cat in separate div

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 'use strict'
 
-let clicks = document.querySelector('#clicks');
-let catPic = document.querySelector('#cat-pic');
+let initCat = function(catDiv, name) {
+  catDiv.querySelector('.cat-name').textContent = name;
 
-clicks.textContent = 0;
+  let catClicks = catDiv.querySelector('.cat-clicks');
+  catClicks.textContent = 0;
 
-catPic.addEventListener('click', function(event) {
-  clicks.textContent = parseInt(clicks.textContent) + 1;
-});
+  catDiv.querySelector('.cat-pic').addEventListener('click', function(event) {
+    incrementClickCounter(catClicks);
+  });
+};
+
+let incrementClickCounter = function(clickCounterElement) {
+  clickCounterElement.textContent = parseInt(clickCounterElement.textContent) + 1;
+};
+
+
+let cat1 = document.querySelector('#cat1');
+let cat2 = document.querySelector('#cat2');
+
+initCat(cat1, 'Hanni');
+initCat(cat2, 'Nanni');
